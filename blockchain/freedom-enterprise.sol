31aa62eb97b3bfbe023bc21dfbdb475a8a22924e
monique <michael.spengler@fh-hwz.ch>
2024-01-05 01:54:34 +0100

    prepare fun

--- blockchain/freedom-enterprise.sol
+++ blockchain/freedom-enterprise.sol
@@ -1,20 +1,17 @@
-/* SPDX-License-Identifier: GNU AFFERO GENERAL PUBLIC LICENSE Version 3
-Solve The Tasks You Like. Delegate The Rest. 
-The Freedom Enterprise allows for free peer to peer collaboration. */
+// SPDX-License-Identifier: GNU AFFERO GENERAL PUBLIC LICENSE Version 3
+// Solve The Tasks You Like. Delegate The Rest. 
+// The Freedom Enterprise is an enterprise system that's fully peer-to-peer, with no trusted third party.
+// The Freedom Enterprise uses Freedom Cash as its decentralized currency
+// https://zkevm.polygonscan.com/token/0xa1e7bB978a28A30B34995c57d5ba0B778E90033B
 
 pragma solidity 0.8.19;
+
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v4.9.4/contracts/utils/math/Math.sol";
 import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/v4.9.4/contracts/token/ERC20/IERC20.sol";
-
-interface IFreedomCash {
-    function getBuyPrice(uint256 amountToBeBought) external view returns(uint256);
-    function getSellPrice() external  view returns(uint256);
-    function buyFreedomCash(uint256 fCAmount, uint256 fCBuyPrice) external payable;
-    function sellFreedomCash(uint256 amount, uint256 sellPrice) external;
-}
+import "https://github.com/monique-baumann/freedom-cash/blob/v1.3.0/blockchain/freedom-cash-interface.sol";
 
 contract FreedomEnterprise {
-  
+
   uint256 public taskCounter = 0;
   uint256 public fundingCounter = 0;
   uint256 public rewardCounter = 0;
@@ -41,12 +38,13 @@ contract FreedomEnterprise {
   mapping(uint256 => uint256) public fundingsToTask;
   mapping(uint256 => uint256) public rewardsToTask;
   address public freedomCashSmartContract = 0x1E7A208810366D0562c7Ba93F883daEedBf31410;
+
   error TaskIDNotAvailableYet();
   error OnlyTheCreatorOfTheTaskCanDoThat();  
   error YouCannotRewardWithMoreThanFunding();
   error NothingToClaimATM();
   error HundredPercentIsEnough();
-
+  
   function createTask(string memory descriptionInMarkdown, uint256 fundingAmountFC) public payable {
     taskCounter++;
     uint256 fCBuyPrice = IFreedomCash(freedomCashSmartContract).getBuyPrice(10**18);
@@ -74,12 +72,21 @@ contract FreedomEnterprise {
     rewards[rewardCounter] = reward;
     rewardsToTask[rewardCounter] = taskID;
   }
+  function claimRewards() public {
+    uint256 totalClaimableRewards = getClaimableRewardAmountForReceiver(msg.sender);
+    if (totalClaimableRewards == 0) { revert NothingToClaimATM(); }
+    IERC20(freedomCashSmartContract).transfer(msg.sender, totalClaimableRewards);
+    for (uint256 i = 1; i <= rewardCounter; i++) {
+      if(rewards[i].to == msg.sender) {
+        rewards[i].claimed = true;
+      }
+    }     
+  }  
   function setCompletionLevel(uint256 taskID, uint24 completionLevel) public {
     if (tasks[taskID].createdBy != msg.sender) { revert OnlyTheCreatorOfTheTaskCanDoThat(); }
     if(completionLevel > 100) { revert HundredPercentIsEnough(); }
     tasks[taskID].completionLevel = completionLevel;
   }
-  
   function getFundingAmountOf(uint256 taskID) public view returns(uint256){
     uint256 total = 0;
     for (uint256 i = 1; i <= fundingCounter; i++) {
@@ -98,14 +105,4 @@ contract FreedomEnterprise {
     }
     return total;
   }  
-  function claimRewards() public {
-    uint256 totalClaimableRewards = getClaimableRewardAmountForReceiver(msg.sender);
-    if (totalClaimableRewards == 0) { revert NothingToClaimATM(); }
-    IERC20(freedomCashSmartContract).transfer(msg.sender, totalClaimableRewards);
-    for (uint256 i = 1; i <= rewardCounter; i++) {
-      if(rewards[i].to == msg.sender) {
-        rewards[i].claimed = true;
-      }
-    }     
-  }
 }
